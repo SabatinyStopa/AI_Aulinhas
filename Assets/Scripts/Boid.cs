25093e8aeb9c6aae4fbbdbd54ac28a15fe1de879
Sabatiny Gabriel Stopa <sabatinystopa@gmail.com>
2020-08-31 22:41:56 -0300

    Flocking -- Boid

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour{
+    public FlockManager m_Manager;
+
+    public float m_Speed;
+    private bool m_Turning;
+
+    private void Start() {
+        m_Speed = Random.Range(m_Manager.m_MinSpeed, m_Manager.m_MaxSpeed);
+    }
+
+    private void Update() {
+        var bounds = m_Manager.Bounds;
+        var hit = new RaycastHit();
+        var direction = Vector3.zero;
+
+        if(!bounds.Contains(transform.position)){
+            m_Turning = true;
+            direction = m_Manager.Bounds.center - transform.position;
+        }else if(Physics.Raycast(transform.position, transform.forward * 10.0f, out hit)){
+            m_Turning = true;
+            direction = Vector3.Reflect(transform.forward, hit.normal);
+        }else{
+            m_Turning = false;
+        }
+
+        if(m_Turning){
+            BackHome(direction);
+        }else{
+            ChangeSpeed();
+            ApplyRules();
+        }
+
+        transform.Translate(0, 0, m_Speed * Time.deltaTime);
+    }
+
+    private void BackHome(Vector3 direction){
+        var rotation = Random.Range(m_Manager.m_MinRotationSpeed, m_Manager.m_MaxSpeed);
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                Quaternion.LookRotation(direction),
+                rotation * Time.deltaTime
+            );
+    }
+
+    private void ChangeSpeed(){
+        if(Random.Range(0.0f, 100.0f) > 10.0f) return;
+        m_Speed = Random.Range(m_Manager.m_MinSpeed, m_Manager.m_MaxSpeed);
+    }
+
+    private void ApplyRules(){
+        if(Random.Range(0.0f, 100.0f) > 20.0f) return;
+
+        var cohesion = Vector3.zero;
+        var separation = Vector3.zero;
+        var groupSize = 0;
+        var distance = 0.0f;
+        var speed = 0.01f;
+
+        foreach (var boid in m_Manager.m_Boids){
+            if(boid == gameObject) continue;
+
+            distance = Vector3.Distance(transform.position, boid.transform.position);
+            if(distance <= m_Manager.m_NeighbourDistance) continue;
+
+            groupSize++;
+            cohesion += boid.transform.position;
+
+            if(distance < 1.0f){
+                separation += transform.position - boid.transform.position;
+            }
+
+            speed += boid.GetComponent<Boid>().m_Speed;
+        }
+
+        if(groupSize == 0) return;
+
+        cohesion = cohesion / groupSize + (m_Manager.m_Target - transform.position);
+        m_Speed = speed / groupSize;
+
+        var direction = (cohesion + separation) - transform.position;
+        if(direction == Vector3.zero) return;
+
+        var rotationSpeed = Random.Range(m_Manager.m_MinRotationSpeed, m_Manager.m_MaxSpeed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/FlockManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class FlockManager : MonoBehaviour{
+    [Header("Flocking")]
+    public GameObject[] m_BoidPrefabs;
+    public int m_NumberBoids = 20;
+    public List<GameObject> m_Boids = new List<GameObject>();
+    public Vector3 m_Target = Vector3.zero;
+    private Collider m_Collider;
+    public Bounds Bounds => m_Collider.bounds;
+
+    [Header("Boids")]
+    [Range(0.0f, 5.0f)]
+    public float m_MinSpeed = 0.4f;
+    [Range(0.0f, 5.0f)]
+    public float m_MaxSpeed = 1.0f;
+    [Range(0.0f, 10.0f)]
+    public float m_MinRotationSpeed = 3.0f;
+    [Range(0.0f, 10.0f)]
+    public float m_MaxRotationSpeed = 6.0f;
+    [Range(0.0f, 10.0f)]
+    public float m_NeighbourDistance = 0.5f;
+
+    private void Start(){
+        m_Collider = GetComponent<Collider>();
+        m_Collider.isTrigger = true;
+        var limits = m_Collider.bounds;
+        for (int i = 0; i < m_NumberBoids; i++)
+        {
+            var position = transform.position;
+            position.x += Random.Range(limits.min.x, limits.max.x);
+            position.y += Random.Range(limits.min.y, limits.max.y);
+            position.z += Random.Range(limits.min.z, limits.max.z);
+
+
+            var index = Random.Range(0, m_BoidPrefabs.Length);
+            var boid = Instantiate(m_BoidPrefabs[index], position, Quaternion.identity, transform);
+            boid.GetComponent<Boid>().m_Manager = this;
+            m_Boids.Add(boid);
+        }
+
+        m_Target = transform.position;
+    }
+
+    private void Update() {
+        if(Random.Range(0.0f, 100.0f) > 10.0f) return;
+        var limits = m_Collider.bounds;
+
+        var position = transform.position;
+        position.x += Random.Range(limits.min.x, limits.max.x);
+        position.y += Random.Range(limits.min.y, limits.max.y);
+        position.z += Random.Range(limits.min.z, limits.max.z);
+
+        m_Target += position;
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(m_Target, 0.5f);    
+    }
+}
